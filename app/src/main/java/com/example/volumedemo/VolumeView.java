56d642764d3475c10ae8996abd1b95f5781ae49f
zhu <zhu@gmail.com>
2017-12-21 14:15:43 +0800

    去掉VolumeDemo 最左侧音量图标。加入宽高适配。

--- app/src/main/java/com/example/volumedemo/VolumeView.java
+++ app/src/main/java/com/example/volumedemo/VolumeView.java
@@ -1,15 +1,17 @@
 package com.example.volumedemo;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+import android.view.WindowManager;
 
 /**
  * 自定义音量显示控件
@@ -28,12 +30,13 @@ public class VolumeView extends View {
     // 小喇叭图片
     private Bitmap volume;
     private Paint paint = new Paint();
-    // 控件高度
+//    // 控件高度
     private int height = 150;
+
     // 控件宽度
     private int width = 930;
     // 最大音量
-    private int MAX = 25;
+    private int MAX = 20;
     // 两个音量矩形最左侧之间的间隔
     private int rectMargen = 25;
     // 音量矩形高
@@ -44,44 +47,69 @@ public class VolumeView extends View {
     private int current = 0;
     // 最左侧音量矩形距离控件最左侧距离
     private int leftMargen = 0;
+    private Context context;
 
     public VolumeView(Context context) {
         super(context);
-        init();
+//        init(context);
     }
 
     public VolumeView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context,attrs);
     }
 
     public VolumeView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context, attrs);
     }
 
 
-    private void init() {
-        addBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.add);
-        volume = BitmapFactory.decodeResource(getResources(), R.drawable.volice);
+    private void init(Context context, AttributeSet attrs) {
+        this.context = context;
+        addBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.add);//+号
+        volume = BitmapFactory.decodeResource(getResources(), R.drawable.volice);//声音图标
         reduceBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.delete);
 //        leftMargen = volume.getWidth() + reduceBitmap.getWidth();//音量图标和减音量图标的宽度
-        leftMargen = volume.getWidth() + reduceBitmap.getWidth() + volume.getWidth() / 4;//音量图标和减音量图标的宽度
+//        leftMargen = volume.getWidth() + reduceBitmap.getWidth() + volume.getWidth() / 4;//音量图标和减音量图标的宽度
 
+
+        height = DisplayUtil.dip2px(context,75);
+//        width = DisplayUtil.dip2px(context,460);
+        width = 4 * getScreenWidth() / 5 + getScreenWidth() / 10;//控件高度
+        leftMargen = getScreenWidth() / 5 / 4;//控件左边距
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.VolumeView);
+
+        rectMargen = (int) a.getDimension(R.styleable.VolumeView_rectMargen, DisplayUtil.dip2px(context,(float)12.5));
+        recW = (int) a.getDimension(R.styleable.VolumeView_recW, DisplayUtil.dip2px(context,(float)7.5));
+        rectH = (int) a.getDimension(R.styleable.VolumeView_rectH, DisplayUtil.dip2px(context,(float)35));//
     }
+    /**
+     * 得到屏幕宽度
+     * @return
+     */
+    private int getScreenWidth() {
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         // 绘制背景颜色
         paint.setColor(getResources().getColor(R.color.white));
-        canvas.drawRect(0, 0, width, height, paint);
+//        canvas.drawRect(0, 0, width, height, paint);//白色背景框
 
         // 绘制没有被选中的白色音量矩形
         paint.setColor(getResources().getColor(R.color.green));
         for (int i = current; i < MAX; i++) {
-            canvas.drawRect(leftMargen + (i + 2) * rectMargen, (height - rectH) / 2, leftMargen + (i + 2) * rectMargen + recW, (height - rectH) / 2 + rectH,
+            canvas.drawRect(leftMargen + (i + 2) * rectMargen, (height - rectH) / 2,
+                            leftMargen + (i + 2) * rectMargen + recW, (height - rectH) / 2 + rectH,
                     paint);
+            //left,top,right,bottom
         }
 
         // 绘制被选中的橘黄色音量矩形
@@ -90,14 +118,15 @@ public class VolumeView extends View {
             canvas.drawRect(leftMargen + (i + 2) * rectMargen, (height - rectH) / 2, leftMargen + (i + 2) * rectMargen + recW, (height - rectH) / 2 + rectH,
                     paint);
         }
-        // 绘制音量图片
-//        canvas.drawBitmap(volume, volume.getWidth() / 2, (height - volume.getHeight()) / 2, paint);
-        canvas.drawBitmap(volume,volume.getWidth() / 2,(height - volume.getHeight()) / 2, paint);//绘制音量图片
+
+//        // 绘制音量图片
+////        canvas.drawBitmap(volume, volume.getWidth() / 2, (height - volume.getHeight()) / 2, paint);
+//        canvas.drawBitmap(volume,volume.getWidth() / 2,(height - volume.getHeight()) / 2, paint);//绘制音量图片
 
 //        // 绘制音量减少图片
 //        canvas.drawBitmap(reduceBitmap, reduceBitmap.getWidth() + volume.getWidth(), (height - reduceBitmap.getHeight()) / 2, paint);
         //绘制音量减少图片
-        canvas.drawBitmap(reduceBitmap,reduceBitmap.getWidth() + volume.getWidth(), (height - reduceBitmap.getHeight()) / 2,paint);
+        canvas.drawBitmap(reduceBitmap,reduceBitmap.getWidth(), (height - reduceBitmap.getHeight()) / 2,paint);
 
         // 绘制音量增加图片
         canvas.drawBitmap(addBitmap, leftMargen + (MAX + 2) * rectMargen, (height - addBitmap.getHeight()) / 2, paint);
@@ -117,7 +146,7 @@ public class VolumeView extends View {
                     }
                     Log.d(TAG, "current:" + current);
                 } else if (event.getX() > leftMargen + (MAX + 1) * rectMargen + recW){//点击加区域时候响应加事件
-                    if (current < 25) {
+                    if (current < 20) {
                         current += 1;
                         if (onChangeListener != null) {
                             onChangeListener.onChange(current);
@@ -149,6 +178,11 @@ public class VolumeView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         setMeasuredDimension(width, height);
 
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
     }
 
     public interface OnChangeListener {
